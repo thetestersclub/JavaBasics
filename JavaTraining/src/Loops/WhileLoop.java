9fe62205c079ee90df355bfe941fe30d757b80d8
Admin <Admin@DESKTOP-6D676A9>
2023-12-08 19:22:50 +0530

    assignment code

--- JavaTraining/src/Loops/WhileLoop.java
+++ JavaTraining/src/Loops/WhileLoop.java
@@ -4,12 +4,23 @@ public class WhileLoop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i = 5;
+		int j = 0;
 		//until the loop condition gets false the loop will be executing
-		while(i<=10) {
-			System.out.println(i);
-			i++;
-			
+//		while(i<=10) {
+//			System.out.println(i);
+//			i++;
+//			
+//		}
+		
+		while(j<=100) {
+			if(j%3 == 0) {
+				System.out.println(j);
+			}
+			j++;
 		}
+		
+		
+		
 	   
 	}
 
@@ -17,4 +28,4 @@ public class WhileLoop {
 
 
 //Assignment 2
-//Use while and do-while to print numbers which are multiple of 5 from 1-100
+//Use while and do-while to print numbers which are multiple of 3 from 1-100
